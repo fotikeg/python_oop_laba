4c74b75c643856b9c6bdb9c58af2e1cbdbc2265d
egorprivet <gthdfuu@gmail.com>
2025-06-17 15:14:01 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,15 @@
+class Employee:
+    name = None
+    age = None
+    salary = None
+    pass
+
+people = Employee()
+
+people.name = "Егор"
+people.age = 17
+people.salary = "500000"
+
+print(people.name)
+print(people.age)
+print(people.salary)
