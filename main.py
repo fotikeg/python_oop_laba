c8bec972ad070d787ef7b00a686728c6103b63c5
egorprivet <gthdfuu@gmail.com>
2025-06-17 15:29:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+class Car:
+    pass
+
+class Car:
+    color = None
+    fuel = None
+    speed = None
+    weight = None
+def go(self):
+  pass
+
+def turn(self):
+  pass
+
+def stop(self):
+  pass
+
+myCar = Car()
+myCar.color = 'red'
+print("Цвет -",myCar.color)
+myCar.fuel = 50
+print("Топливо -",myCar.fuel)
+myCar.speed = 100
+print("Скорость -",myCar.speed)
+myCar.weight = 1000
+print("Вес -",myCar.weight)
