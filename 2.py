38337201a162b664a65bac0919e31f1eaff63cd1
egorprivet <gthdfuu@gmail.com>
2025-06-17 15:13:31 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+class Employee:
+    name = None
+    age = None
+    city = None
+    pass
+
+people = Employee()
+
+people.name = "Егор"
+people.age = 17
+people.city = "Вологда"
+
+print(people.name)
+print(people.age)
+print(people.city)
